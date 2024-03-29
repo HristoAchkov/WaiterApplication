e813387c671429f2d6040bc5a489d68c403bbea7
HristoAchkov <achkovh@yahoo.com>
2024-03-29 16:29:45 +0200

    Fixed references and built the database.

--- WaiterApplication.Infrastructure/Data/ApplicationDbContext.cs
+++ WaiterApplication.Infrastructure/Data/ApplicationDbContext.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
-namespace WaiterApplication.Data
+namespace WaiterApplication.Infrastructure.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
