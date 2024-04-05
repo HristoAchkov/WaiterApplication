9f29348bc9af10d016c635c1badd8a572ec3a856
HristoAchkov <achkovh@yahoo.com>
2024-04-05 22:50:41 +0300

    Created a new attribute and a new model.

---
+++ WaiterApplication.Core/Models/CreateDishModel.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using static WaiterApplication.Infrastructure.Constants.DataConstants;
+
+namespace WaiterApplication.Core.Models
+{
+    public class CreateDishModel
+    {
+        [Required]
+        [MinLength(MinNameLength)]
+        [MaxLength(MaxNameLength)]
+        public string Name { get; set; } = string.Empty;
+        [Required]
+        [Description]
+        [MinLength(MinDescriptionLength)]
+        [MaxLength(MaxDescriptionLength)]
+        public string Description { get; set; } = string.Empty;
+        [Required]
+        [Url]
+        [MaxLength(MaxUrlLength)]
+        public string Image { get; set; } = string.Empty;
+        [Required]
+        [Range(0.00, 1000.00, ErrorMessage = PriceRange)]
+        public decimal Price { get; set; }
+        public string? Ingredients { get; set; }
+    }
+}
