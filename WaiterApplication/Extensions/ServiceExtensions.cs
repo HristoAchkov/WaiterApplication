3e03a22c128dcedd49f3fc328f2c6338c7fb2098
HristoAchkov <achkovh@yahoo.com>
2024-04-05 14:21:03 +0300

    Added Claims extension.

--- WaiterApplication/Extensions/ServiceExtensions.cs
+++ WaiterApplication/Extensions/ServiceExtensions.cs
@@ -12,6 +12,7 @@ namespace Microsoft.Extensions.DependencyInjection
         public static IServiceCollection AddApplicationServices(this IServiceCollection services)
         {
             services.AddScoped<IOrderService, OrderService>();
+            services.AddScoped<IDishService, DishService>();
 
             return services;
         }
