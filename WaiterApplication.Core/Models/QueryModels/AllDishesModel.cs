4f14a5ddf9af0f9615740291ae8cb02b54fc902d
HristoAchkov <achkovh@yahoo.com>
2024-04-08 22:21:14 +0300

    Added Dish models.

---
+++ WaiterApplication.Core/Models/QueryModels/AllDishesModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WaiterApplication.Core.Models.QueryModels
+{
+    public class AllDishesModel
+    {
+        public int TotalDishesCount { get; set; }
+        public IEnumerable<AllDishesServiceModel> Dishes { get; set; } = new HashSet<AllDishesServiceModel>();
+    }
+}
---
+++ WaiterApplication.Core/Models/QueryModels/AllDishesServiceModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static WaiterApplication.Infrastructure.Constants.DataConstants;
+
+namespace WaiterApplication.Core.Models.QueryModels
+{
+    public class AllDishesServiceModel
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; } = string.Empty;
+        [Required]
+        public string Description { get; set; } = string.Empty;
+        public string? Image { get; set; }
+        [Required]
+        public decimal Price { get; set; }
+        public string? Ingredients { get; set; }
+    }
+}
