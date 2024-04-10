7bd8b7530edb8ee02e98844d9415504b173a72f6
HristoAchkov <achkovh@yahoo.com>
2024-04-11 00:15:02 +0300

    Added Error pages.

--- WaiterApplication/Controllers/HomeController.cs
+++ WaiterApplication/Controllers/HomeController.cs
@@ -21,9 +21,18 @@ namespace WaiterApplication.Controllers
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+        public IActionResult Error(int statusCode)
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            if (statusCode == 404)
+            {
+                return View("Error404");
+            }
+            if (statusCode == 500)
+            {
+                return View("Error500");
+            }
+
+            return View();
         }
     }
 }
