a87468331bbacac916348d6a168c8f4a317c0d74
HristoAchkov <achkovh@yahoo.com>
2024-04-13 21:39:00 +0300

    Migrations.

---
+++ WaiterApplication.Infrastructure/Migrations/20240408153653_migration.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WaiterApplication.Infrastructure.Migrations
+{
+    public partial class migration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Image",
+                table: "Dishes",
+                type: "nvarchar(200)",
+                maxLength: 200,
+                nullable: true,
+                comment: "Image URL",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(200)",
+                oldMaxLength: 200,
+                oldComment: "Image URL");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Image",
+                table: "Dishes",
+                type: "nvarchar(200)",
+                maxLength: 200,
+                nullable: false,
+                defaultValue: "",
+                comment: "Image URL",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(200)",
+                oldMaxLength: 200,
+                oldNullable: true,
+                oldComment: "Image URL");
+        }
+    }
+}
