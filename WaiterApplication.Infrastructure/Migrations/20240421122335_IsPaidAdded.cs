48cb8495339530733fd3d338d1046dab1cbd8564
HristoAchkov <achkovh@yahoo.com>
2024-04-22 13:05:03 +0300

    Migrations.

---
+++ WaiterApplication.Infrastructure/Migrations/20240421122335_IsPaidAdded.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WaiterApplication.Infrastructure.Migrations
+{
+    public partial class IsPaidAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Dishes_Orders_OrderId",
+                table: "Dishes");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Dishes_OrderId",
+                table: "Dishes");
+
+            migrationBuilder.DropColumn(
+                name: "OrderId",
+                table: "Dishes");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsPaid",
+                table: "Orders",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsPaid",
+                table: "Orders");
+
+            migrationBuilder.AddColumn<int>(
+                name: "OrderId",
+                table: "Dishes",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Dishes_OrderId",
+                table: "Dishes",
+                column: "OrderId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Dishes_Orders_OrderId",
+                table: "Dishes",
+                column: "OrderId",
+                principalTable: "Orders",
+                principalColumn: "Id");
+        }
+    }
+}
