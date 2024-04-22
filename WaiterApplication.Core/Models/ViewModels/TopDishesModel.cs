0b79553617f92857785f6f6dbed7c0753a9809e7
HristoAchkov <achkovh@yahoo.com>
2024-04-22 10:38:46 +0300

    Added Get Report functionality.

---
+++ WaiterApplication.Core/Models/ViewModels/TopDishesModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WaiterApplication.Core.Models.ViewModels
+{
+    public class TopDishesModel
+    {
+        public List<string> DishNames { get; set; } = new List<string>();
+        public List<int> TimesOrdered { get; set; } = new List<int>();
+    }
+}
