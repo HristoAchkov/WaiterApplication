1283ca9b292ffd6d279a769eac171e10b9643bde
HristoAchkov <achkovh@yahoo.com>
2024-05-31 17:50:13 +0300

    Added Promotion and PromoDish entity models.

---
+++ WaiterApplication.Infrastructure/Data/Models/PromoDish.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WaiterApplication.Infrastructure.Data.Models
+{
+    public class PromoDish
+    {
+        [Comment("Promotion Dish Identifier")]
+        public int Id { get; set; }
+        [Required]
+        public int PromotionId { get; set; }
+        [ForeignKey(nameof(PromotionId))]
+        public Promotion Promotion { get; set; } = null!;
+        [Required]
+        public int DishId { get; set; }
+        [ForeignKey(nameof(DishId))]
+        public Dish Dish { get; set; } = null!;
+        [DefaultValue(1)]
+        public int Quantity { get; set; }
+    }
+}
---
+++ WaiterApplication.Infrastructure/Data/Models/Promotion.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WaiterApplication.Infrastructure.Data.Models
+{
+    public class Promotion
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; } = string.Empty;
+        [Required]
+        public decimal Price { get; set; }
+        public List<PromoDish> Dishes { get; set; } = new List<PromoDish>();
+        public int? OrderId { get; set; }
+        [ForeignKey(nameof(OrderId))]
+        public virtual Order? Order { get; set; }
+    }
+}
