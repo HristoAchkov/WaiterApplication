7bf8d929843ae05bae05e1448cc4d38692ca2fde
HristoAchkov <achkovh@yahoo.com>
2024-05-31 17:56:10 +0300

    CSS for a Promotion page added. A view model created and a minor change to another.

---
+++ WaiterApplication.Core/Models/ViewModels/AllPromotionsViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WaiterApplication.Infrastructure.Data.Models;
+
+namespace WaiterApplication.Core.Models.ViewModels
+{
+    public class AllPromotionsViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public List<PromoDish> Dishes { get; set; } = null!;
+    }
+}
